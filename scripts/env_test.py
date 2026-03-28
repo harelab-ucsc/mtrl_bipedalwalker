fa021771396722ec803fc2c21aecd0104e9b33d2
Lemon Foxmere <lemon@lemonfoxmere.com>
2026-03-27 22:40:12 -0700

    added keyboard controls, fixed visualization ordering

--- scripts/env_test.py
+++ scripts/env_test.py
@@ -1,19 +1,35 @@
 import numpy as np
 from gymnasium import make
+from pynput import keyboard
+from pynput.keyboard import Key, KeyCode
+import os
 
-from lib.wrappers.bipedal_walker.test import Test_Wrapper
+from wrappers.bipedal_walker.plot_env import Plot_Env
 
 SEED = 42
 
+_sim_paused = False
+_sim_step = False
+
+
 def main():
+    global _sim_paused, _sim_step
     
     env = make("BipedalWalker-v3", render_mode="human")
-    wrap_env = Test_Wrapper(env, plotting=True)
+    wrap_env = Plot_Env(env)
     wrap_env.reset(seed=SEED)
     wrap_env.action_space.seed(SEED)
     
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()  # start to listen on a separate thread
     
-    while(1):    
+    while(1):
+        if _sim_paused:
+            if not _sim_step:
+                continue
+            else:
+                _sim_step = False
+        
         assert wrap_env.action_space.shape is not None
         
         # random agent
@@ -22,6 +38,27 @@ def main():
         print("=== Testing with action: ", action)
         # print()
         # print();
+
+
+def on_press(key: Key | KeyCode | None) -> None:
+    global _sim_paused, _sim_step
+    
+    if isinstance(key, KeyCode):
+        k = key.char
+    elif isinstance(key, Key):
+        k = key.name
+    else:
+        return
+    
+    if k == 'space':
+        _sim_paused = not _sim_paused
+        print("Paused" if _sim_paused else "Resumed")
+    elif k == 's':
+        _sim_step = True
+    elif k == 'q':
+        print("Exiting...")
+        os._exit(0)
+
     
 if __name__ == "__main__":
     main()
--- scripts/sb3/play.py
+++ scripts/sb3/play.py
@@ -1,8 +1,12 @@
+import os
 import gymnasium as gym
 from stable_baselines3 import PPO
 
-from lib.utils.paths import MODELS_DIR
-from lib.wrappers.bipedal_walker.test import Test_Wrapper
+from pynput import keyboard
+from pynput.keyboard import Key, KeyCode
+
+from utils.paths import MODELS_DIR
+from wrappers.bipedal_walker.plot_env import Plot_Env
 
 # =========================================
 
@@ -11,8 +15,11 @@ MODEL_CHECKPOINT = "best/best_model"
 
 # =========================================
 
+_sim_paused = False
+_sim_step = False
+
 env = gym.make("BipedalWalker-v3", render_mode="human")
-wrap_env = Test_Wrapper(env, plotting=True)
+wrap_env = Plot_Env(env)
 wrap_env.reset()
 
 model_path = MODELS_DIR / f"{EXPERIMENT_NAME}/{MODEL_CHECKPOINT}.zip"
@@ -20,13 +27,50 @@ model = PPO.load(model_path, env=wrap_env)
 
 episodes = 5
 
-for ep in range(episodes):
-    obs, _ = wrap_env.reset()
-    done = False
-    total_rewards = 0
-    while not done:
-        action, _states = model.predict(obs)
-        obs, rewards, done, info, _ = wrap_env.step(action)
-        total_rewards += float(rewards)
-        wrap_env.render()
-    print(total_rewards)
+def main():
+    global _sim_paused, _sim_step
+    
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()  # start to listen on a separate thread
+    
+    for ep in range(episodes):
+        obs, _ = wrap_env.reset()
+        done = False
+        total_rewards = 0
+        
+        while not done:
+            if _sim_paused:
+                if not _sim_step:
+                    continue
+                else:
+                    _sim_step = False
+                
+            action, _states = model.predict(obs)
+            obs, rewards, done, info, _ = wrap_env.step(action)
+            total_rewards += float(rewards)
+            wrap_env.render()
+        print(total_rewards)
+
+
+def on_press(key: Key | KeyCode | None) -> None:
+    global _sim_paused, _sim_step
+    
+    if isinstance(key, KeyCode):
+        k = key.char
+    elif isinstance(key, Key):
+        k = key.name
+    else:
+        return
+    
+    if k == 'space':
+        _sim_paused = not _sim_paused
+        print("Paused" if _sim_paused else "Resumed")
+    elif k == 's':
+        _sim_step = True
+    elif k == 'q':
+        print("Exiting...")
+        os._exit(0)
+
+
+if __name__ == "__main__":
+    main()
